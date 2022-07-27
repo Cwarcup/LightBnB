fe7a9e287e48050cf9a88cfc551fef5e176adbf8
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-27 09:36:30 -0700

    Seeds insert into lightBNB database

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,14 +1,23 @@
 INSERT INTO users (name, email, password)
 VALUES ('Curtis Warcup', 'curtis@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' ),
-VALUES ('Hana Ito', 'hana@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' ),
-VALUES ('Gilligan Dog', 'gilly@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' );
+('Hana Ito', 'hana@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' ),
+('Gilligan Dog', 'gilly@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.' );
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, postal_code, active)
-VALUES (1, 'The Beach House', 'A beautiful beach house', 'https://images.unsplash.com/photo-1511840636560-acee95b3a83f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80', 'https://images.unsplash.com/photo-1511840636560-acee95b3a83f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80', 100, 2, 1, 1, 'Canada', '123 Main St', 'Vancouver', 'BC', 'V6J1H3', TRUE),
-VALUES (2, 'The Lake House', 'A beautiful Lake house', 'https://images.unsplash.com/photo-1559767949-0faa5c7e9992?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1559767949-0faa5c7e9992?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 300, 4, 3, 3, 'Canada', '123 Victoria Dr', 'Richmond', 'BC', 'V7G2H9', TRUE),
-VALUES (3, 'The Dog House', 'A dog house', 'https://images.unsplash.com/photo-1599427294074-524c52911d53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1599427294074-524c52911d53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 15, 0, 1, 1, 'Canada', '123 Dyke Rd', 'Richmond', 'BC', 'V7C2B6', FALSE);
+VALUES 
+(1, 'The Beach House', 'A beautiful beach house', 'https://images.unsplash.com/photo-1511840636560-acee95b3a83f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80', 'https://images.unsplash.com/photo-1511840636560-acee95b3a83f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80', 100, 2, 1, 1, 'Canada', '123 Main St', 'Vancouver', 'BC', 'V6J1H3', TRUE),
+(2, 'The Lake House', 'A beautiful Lake house', 'https://images.unsplash.com/photo-1559767949-0faa5c7e9992?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1559767949-0faa5c7e9992?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 300, 4, 3, 3, 'Canada', '123 Victoria Dr', 'Richmond', 'BC', 'V7G2H9', TRUE),
+(3, 'The Dog House', 'A dog house', 'https://images.unsplash.com/photo-1599427294074-524c52911d53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 'https://images.unsplash.com/photo-1599427294074-524c52911d53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 15, 0, 1, 1, 'Canada', '123 Dyke Rd', 'Richmond', 'BC', 'V7C2B6', FALSE);
 
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES 
+('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
 
-
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES
+(1, 2, 3, 5, 'Small place'),
+(2, 3, 1, 1, 'Was poop in the yard'),
+(3, 1, 2, 8, 'Small place');
