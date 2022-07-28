17a6190cb206bbca39501d4ded70bf02dccca426
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-07-27 20:40:37 -0700

    Update getUserWithEmail to use db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -25,16 +25,15 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool
+    .query('SELECT * FROM users WHERE email = $1', [email])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -86,7 +85,6 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query('SELECT * FROM properties LIMIT $1', [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
